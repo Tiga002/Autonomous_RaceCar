e5ea9e7448c5bf92053dc6fe1f5c5d25abb426f9
leunghoyin <leunghoyin@sensetime.com>
2018-10-24 14:42:48 +0800

    Server Side Setup Guide Finished git add -A! ^^

---
+++ RaceCar_Client/encoder_odom_creater.cpp
@@ -0,0 +1,99 @@
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <nav_msgs/Odometry.h>
+#include <std_msgs/UInt16.h>
+#include <cmath>
+const double PI  =3.141592653589793238463;
+double x_velocity = 0;
+double steering_angle = 0;
+double radius = 0.05;
+void rpm_Callback(const std_msgs::Uint16::ConstPtr& data)
+{
+  //Processed the incoming callbaced msg
+  x_velocity = data.data;
+  x_velocity = static_cast<int>(x_velocity);
+  wheel_circumference = 2*PI*radius;
+  x_velocity = (x_velocity/60)*wheel_circumference;
+}
+
+void steerling_Callback(const std_msgs::Uint16::ConstPtr& data)
+{
+  //Processed the incoming callbaced msg
+  steering_angle = data.data;
+  steering_angle = static_cast<int>(steering_angle);
+}
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "odometry_publisher");
+
+  ros::NodeHandle n;
+  ros::Subscriber encoder_sub= n.subscribe<std_msgs::Uint16>("encoder", 100, rpm_Callback);
+  ros::Subscriber steerling_angle_sub= n.subscribe<std_msgs::Uint16>("servo", 100, steerling_Callback);
+  ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom", 50);
+  tf::TransformBroadcaster odom_broadcaster;
+
+  double x = 0.0;
+  double y = 0.0;
+  double previous_steerling_angle = 0;
+  double vy = 0;
+  ros::Time current_time, last_time;
+  current_time = ros::Time::now();
+  last_time = ros::Time::now();
+
+  ros::Rate r(1.0);
+  while(n.ok()){
+
+    ros::spinOnce();               // check for incoming messages
+    current_time = ros::Time::now();
+    
+    //compute odometry in a typical way given the velocities of the robot
+    double dt = (current_time - last_time).toSec();
+    double delta_x = (x_velocity * cos(steering_angle) * dt;
+    double delta_y = (x_velocity * sin(steering_angle) * dt;
+    double v_th = (steering_angle - previous_steerling_angle)/dt;
+    previous_steerling_angle = steering_angle;
+    x += delta_x;
+    y += delta_y;
+    
+
+    //since all odometry is 6DOF we'll need a quaternion created from yaw
+    geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(steering_angle);
+
+    //first, we'll publish the transform over tf
+    geometry_msgs::TransformStamped odom_trans;
+    odom_trans.header.stamp = current_time;
+    odom_trans.header.frame_id = "odom";
+    odom_trans.child_frame_id = "base_link";
+
+    odom_trans.transform.translation.x = x;
+    odom_trans.transform.translation.y = y;
+    odom_trans.transform.translation.z = 0.0;
+    odom_trans.transform.rotation = odom_quat;
+
+    //send the transform
+    odom_broadcaster.sendTransform(odom_trans);
+
+    //next, we'll publish the odometry message over ROS
+    nav_msgs::Odometry odom;
+    odom.header.stamp = current_time;
+    odom.header.frame_id = "odom";
+
+    //set the position
+    odom.pose.pose.position.x = x;
+    odom.pose.pose.position.y = y;
+    odom.pose.pose.position.z = 0.0;
+    odom.pose.pose.orientation = odom_quat;
+
+    //set the velocity
+    odom.child_frame_id = "base_link";
+    odom.twist.twist.linear.x = x_velocity;
+    odom.twist.twist.linear.y = vy;
+    odom.twist.twist.angular.z = vth;
+
+    //publish the message
+    odom_pub.publish(odom);
+
+    last_time = current_time;
+    r.sleep();
+  }
+}
